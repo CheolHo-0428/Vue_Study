f9669ad344589be599a8b078a7f50ab66f0976b6
CK-0428 <againnr1@gmail.com>
2021-10-07 20:35:46 +0900

    lowdb install

--- vue-todo-app/components/TodoApp.vue
+++ vue-todo-app/components/TodoApp.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import lowdb from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 
@@ -13,6 +15,29 @@ export default {
     components: {
         TodoCreator, 
         TodoItem
+    },
+
+    data () {
+        return {
+            db: null
+        }
+    },
+    
+    created () { //TodoApp이 생성된 직후에 initDB 작동
+      this.initDB()
+    },
+
+    methods: {
+        initDB () {
+            const adapter = new LocalStorage('todo-app') //DB 이름 todo-app
+            this.db = lowdb(adapter)
+            console.log(this.db)
+
+            //localDB 초기화
+            this.db.defaults({
+              todos: []
+            }).write
+        }
     }
 }
 </script>
