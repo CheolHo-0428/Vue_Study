dbec7dd3c587c3f68995a21b72b46aae6b156ccd
CK-0428 <againnr1@gmail.com>
2021-10-08 01:35:28 +0900

    ch 04 예제 2 - 09 CRUD-Read Todos

--- vue-todo-ap/components/TodoCreator.vue
+++ vue-todo-ap/components/TodoCreator.vue
@@ -21,9 +21,16 @@ export default {
     },
     methods: {
       createTodo(){
-        console.log(this.title)
+        const validatedTitle = this.title && this.title.trim()
+        if(!validatedTitle){
+          alert("유효하지 않은 제목입니다.")
+          this.title = this.title.trim()
+          return 
+        }
+
         //생성
         this.$emit('create-todo', this.title)
+        this.title = ''
       }
     }
 }
